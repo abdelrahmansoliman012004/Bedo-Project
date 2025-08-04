70a714c9d1b024c44cc15d9150d7b74807933697
abdelrahmansoliman012004 <mohamedmahmoudabelrahman@gmail.com>
2025-08-05 01:02:59 +0300

    updated files and the first version of the code

--- python_script.py
+++ python_script.py
@@ -1,5 +1,9 @@
 import os
-import pandas as pd
+import pandas as pd # pyright: ignore[reportMissingModuleSource]
 
-df = pd.read_csv(r"D:\Bedo project\Student_performance_data _.csv")
-
+df = pd.read_csv(r"D:\Bedo project\student_dataset.csv") # Read CSV file
+df.drop(columns=["Phone_No.", "Roll No.", "School Name", "Student Address"], inplace=True) # Drop unneeded columns (data cleaning)
+print(df.columns)
+print(df.head())
+#print(df.info()) # Show DataFrame structure
+#print(df.describe()) # Show summary statistics
